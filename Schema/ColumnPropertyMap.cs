13982c580a2d5b8b8e1d58f6bae20d74bd4909b3
Thomas Andresen <thomas.r.andresen@gmail.com>
2019-09-25 13:07:10 +0200

    Ctors

--- Schema/ColumnPropertyMap.cs
+++
@@ -1,6 +0,0 @@
-using System.Collections.Generic;
-
-namespace Necessity.UnitOfWork.Schema
-{
-    public class ColumnPropertyMap : Dictionary<string, Mapping> { }
-}
--- Schema/PropertyColumnMap.cs
+++ Schema/PropertyColumnMap.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Collections.Generic;
 
 namespace Necessity.UnitOfWork.Schema
 {
-    public class PropertyColumnMap : Dictionary<string, Mapping> { }
+    public class PropertyColumnMap : Dictionary<string, Mapping>
+    {
+        public PropertyColumnMap() : base(StringComparer.OrdinalIgnoreCase) { }
+        public PropertyColumnMap(Dictionary<string, Mapping> dict) : base(dict, StringComparer.OrdinalIgnoreCase) { }
+    }
 }
