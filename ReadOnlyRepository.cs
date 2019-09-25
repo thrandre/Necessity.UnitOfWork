f5e522eaaa8656fabcf8d9fa4403c6cde6b3a421
Thomas Andresen <thomas.r.andresen@gmail.com>
2019-09-25 18:23:48 +0200

    Logging

--- ReadOnlyRepository.cs
+++ ReadOnlyRepository.cs
@@ -4,30 +4,38 @@ using System.Data;
 using System.Linq;
 using Dapper;
 using Necessity.UnitOfWork.Predicates;
+using System;
 
 namespace Necessity.UnitOfWork
 {
     public class ReadOnlyRepository<TEntity, TKey> : IReadOnlyRepository<TEntity, TKey>
     {
-        public ReadOnlyRepository(IDbTransaction transaction, IReadOnlyQueryBuilder<TEntity, TKey> queryBuilder)
+        public ReadOnlyRepository(
+            IDbTransaction transaction,
+            IReadOnlyQueryBuilder<TEntity, TKey> queryBuilder,
+            Action<object> logger = null)
         {
             Transaction = transaction;
             QueryBuilder = queryBuilder;
+            Logger = logger;
         }
 
         public IDbTransaction Transaction { get; }
         public IDbConnection Connection => Transaction.Connection;
         public IReadOnlyQueryBuilder<TEntity, TKey> QueryBuilder { get; }
 
+        protected Action<object> Logger { get; }
+
         public virtual async Task<TEntity> Get(TKey key)
         {
             var queryParams = new Dictionary<string, object>();
+            var query = QueryBuilder.Get(key, queryParams);
+
+            Logger?.Invoke(new { Method = nameof(Get), Query = query, Params = queryParams });
 
             return (await Connection
                 .QueryAsync<TEntity>(
-                    QueryBuilder.Get(
-                        key,
-                        queryParams),
+                    query,
                     queryParams,
                     Transaction))
                 .Single();
@@ -36,10 +44,13 @@ namespace Necessity.UnitOfWork
         public virtual async Task<List<TEntity>> GetAll()
         {
             var queryParams = new Dictionary<string, object>();
+            var query = QueryBuilder.GetAll(queryParams);
+
+            Logger?.Invoke(new { Method = nameof(GetAll), Query = query, Params = queryParams });
 
             return (await Connection
                 .QueryAsync<TEntity>(
-                    QueryBuilder.GetAll(queryParams),
+                    query,
                     queryParams,
                     Transaction))
                 .ToList();
@@ -48,10 +59,13 @@ namespace Necessity.UnitOfWork
         public virtual async Task<List<TEntity>> Find(Predicate predicate)
         {
             var queryParams = new Dictionary<string, object>();
+            var query = QueryBuilder.Find(predicate, queryParams);
+
+            Logger?.Invoke(new { Method = nameof(Find), Query = query, Params = queryParams });
 
             return (await Connection
                 .QueryAsync<TEntity>(
-                    QueryBuilder.Find(predicate, queryParams),
+                    query,
                     queryParams,
                     Transaction))
                 .ToList();
--- Repository.cs
+++ Repository.cs
@@ -2,13 +2,17 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Data;
 using Dapper;
+using System;
 
 namespace Necessity.UnitOfWork
 {
     public class Repository<TEntity, TKey> : ReadOnlyRepository<TEntity, TKey>, IRepository<TEntity, TKey> where TEntity : class, new()
     {
-        public Repository(IDbTransaction transaction, IQueryBuilder<TEntity, TKey> queryBuilder)
-            : base(transaction, queryBuilder)
+        public Repository(
+            IDbTransaction transaction,
+            IQueryBuilder<TEntity, TKey> queryBuilder,
+            Action<object> logger = null)
+            : base(transaction, queryBuilder, logger)
         {
         }
 
@@ -17,10 +21,13 @@ namespace Necessity.UnitOfWork
         public virtual Task Create(TEntity entity)
         {
             var queryParams = new Dictionary<string, object>();
+            var query = QueryBuilder.Create(entity, queryParams);
+
+            Logger?.Invoke(new { Method = nameof(Create), Query = query, Params = queryParams });
 
             return Connection
                 .ExecuteAsync(
-                    QueryBuilder.Create(entity, queryParams),
+                    query,
                     queryParams,
                     Transaction);
         }
@@ -28,10 +35,13 @@ namespace Necessity.UnitOfWork
         public virtual Task Update(TEntity entity)
         {
             var queryParams = new Dictionary<string, object>();
+            var query = QueryBuilder.Update(entity, queryParams);
+
+            Logger?.Invoke(new { Method = nameof(Update), Query = query, Params = queryParams });
 
             return Connection
                 .ExecuteAsync(
-                    QueryBuilder.Update(entity, queryParams),
+                    query,
                     queryParams,
                     Transaction);
         }
@@ -39,10 +49,13 @@ namespace Necessity.UnitOfWork
         public virtual Task Upsert(TEntity entity, OnConflict onConflict)
         {
             var queryParams = new Dictionary<string, object>();
+            var query = QueryBuilder.Upsert(entity, onConflict, queryParams);
+
+            Logger?.Invoke(new { Method = nameof(Upsert), Query = query, Params = queryParams });
 
             return Connection
                 .ExecuteAsync(
-                    QueryBuilder.Upsert(entity, onConflict, queryParams),
+                    query,
                     queryParams,
                     Transaction);
         }
@@ -50,10 +63,13 @@ namespace Necessity.UnitOfWork
         public virtual Task Delete(TKey key)
         {
             var queryParams = new Dictionary<string, object>();
+            var query = QueryBuilder.Delete(key, queryParams);
+
+            Logger?.Invoke(new { Method = nameof(Delete), Query = query, Params = queryParams });
 
             return Connection
                 .ExecuteAsync(
-                    QueryBuilder.Delete(key, queryParams),
+                    query,
                     queryParams,
                     Transaction);
         }
